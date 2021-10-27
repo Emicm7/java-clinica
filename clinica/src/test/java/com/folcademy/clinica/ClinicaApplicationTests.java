ad576da0841bf8fce50a9563de759ce47f6e930b
Ramiro Bernal Albernez <ramirobernal@unsj-cuim.edu.ar>
2021-10-27 18:02:13 -0300

    Creo proyecto nuevo

---
+++ clinica/src/test/java/com/folcademy/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folcademy.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
