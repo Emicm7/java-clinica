6cc0990f7fe06e2366f36ec4962cf352fe7b5202
Emiliano <emilianocastro97@gmail.com>
2021-10-29 03:24:30 -0300

    🔨 Refactorizacion de paciente service

--- clinica/src/main/java/com/folcademy/clinica/Services/PacienteService.java
+++ clinica/src/main/java/com/folcademy/clinica/Services/PacienteService.java
@@ -3,6 +3,7 @@ package com.folcademy.clinica.Services;
 import com.folcademy.clinica.Model.Entities.Paciente;
 import com.folcademy.clinica.Model.Repositories.PacienteRepository;
 import com.folcademy.clinica.Services.Interfaces.IPacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,12 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 @Service
 public class PacienteService implements IPacienteService {
-    private final PacienteRepository pacienteRepository;
 
-    public PacienteService(PacienteRepository pacienteRepository) {
-        this.pacienteRepository = pacienteRepository;
-    }
-
+    @Autowired
+    private PacienteRepository pacienteRepository;
 
     @Override
     public List<Paciente> findAllPacientes() {
