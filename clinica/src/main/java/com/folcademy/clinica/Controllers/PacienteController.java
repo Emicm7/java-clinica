77866f5022378604084ffa0ee8ecc262a2094319
Emiliano <emilianocastro97@gmail.com>
2021-10-29 03:12:24 -0300

    🔨 Refactorizacion de servicio paciente

--- clinica/src/main/java/com/folcademy/clinica/Controllers/PacienteController.java
+++ clinica/src/main/java/com/folcademy/clinica/Controllers/PacienteController.java
@@ -2,6 +2,7 @@ package com.folcademy.clinica.Controllers;
 
 import com.folcademy.clinica.Model.Entities.Paciente;
 import com.folcademy.clinica.Services.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,31 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/paciente")
+@RequestMapping("/pacientes")
 public class PacienteController {
 
-    private final PacienteService pacienteService;
-
-    public PacienteController(PacienteService pacienteService) {
-        this.pacienteService = pacienteService;
-    }
-
+    @Autowired
+    private PacienteService pacienteService;
 
     @GetMapping(value = "")
     public ResponseEntity<List<Paciente>> findAll() {
         return ResponseEntity
                 .ok()
-                .body(
-                        pacienteService.findAllPacientes())
-                ;
+                .body(pacienteService.findAllPacientes());
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<List<Paciente>> findAll(@PathVariable(name = "id") Integer id) {
+    public ResponseEntity<Paciente> getOne(@PathVariable(name = "id") Integer id) {
         return ResponseEntity
                 .ok()
-                .body(
-                        pacienteService.findPacienteById(id))
-                ;
+                .body(pacienteService.findPacienteById(id));
     }
 }
