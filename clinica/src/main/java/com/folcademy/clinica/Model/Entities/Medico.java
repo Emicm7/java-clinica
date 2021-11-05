8d955b2b2ed9691abafab2a19399617385c99fe0
Emiliano <emilianocastro97@gmail.com>
2021-11-05 17:54:45 -0300

    🔨 Refactorizacion de Medico entidad

--- clinica/src/main/java/com/folcademy/clinica/Model/Entities/Medico.java
+++ clinica/src/main/java/com/folcademy/clinica/Model/Entities/Medico.java
@@ -1,19 +1,40 @@
 package com.folcademy.clinica.Model.Entities;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "medico")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idmedico")
-    public Integer idmedico;
+    Integer idmedico;
     @Column(name = "Nombre")
-    public String nombre;
+    String nombre = "";
     @Column(name = "Apellido")
-    public String apellido;
+    String apellido;
     @Column(name = "Telefono")
-    public String telefono;
+    String telefono;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Medico medico = (Medico) o;
+        return idmedico != null && Objects.equals(idmedico, medico.idmedico);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
 
