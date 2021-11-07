f562a2b0782ddce22e0c497cf51c61f056e068e0
Emiliano <emilianocastro97@gmail.com>
2021-11-07 17:06:12 -0300

    🔨 Refactorizacion de Paciente Entity

--- clinica/src/main/java/com/folcademy/clinica/Model/Entities/Paciente.java
+++ clinica/src/main/java/com/folcademy/clinica/Model/Entities/Paciente.java
@@ -1,9 +1,17 @@
 package com.folcademy.clinica.Model.Entities;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "paciente")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +25,17 @@ public class Paciente {
     public String apellido;
     @Column(name = "Telefono", columnDefinition = "VARCHAR")
     public String telefono;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Paciente paciente = (Paciente) o;
+        return idpaciente != null && Objects.equals(idpaciente, paciente.idpaciente);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
