d20081845ce0a646f54aee58d816e7084b1a9c37
Emiliano <emilianocastro97@gmail.com>
2021-11-23 00:10:51 -0300

    🔨 Refactorizacion Interfaz de medico Service

--- clinica/src/main/java/com/folcademy/clinica/Services/Interfaces/IMedicoService.java
+++ clinica/src/main/java/com/folcademy/clinica/Services/Interfaces/IMedicoService.java
@@ -1,6 +1,7 @@
 package com.folcademy.clinica.Services.Interfaces;
 
 import com.folcademy.clinica.Model.Dtos.MedicoDto;
+import com.folcademy.clinica.Model.Dtos.MedicoEnteroDto;
 import com.folcademy.clinica.Model.Entities.Medico;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface IMedicoService {
     List<MedicoDto> findAllMedicos();
     MedicoDto findMedicoById(Integer id);
     MedicoDto save(Medico medico);
+    MedicoEnteroDto edit(Integer idMedico, MedicoEnteroDto dto);
+    MedicoDto delete(Integer id);
 }
