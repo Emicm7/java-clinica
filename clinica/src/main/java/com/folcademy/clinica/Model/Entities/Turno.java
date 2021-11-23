811747c5098f4ea1fd0457a65a498e406dbecbb6
Emiliano <emilianocastro97@gmail.com>
2021-11-23 00:10:03 -0300

    🔨 Refactorizacion En turno Entity

--- clinica/src/main/java/com/folcademy/clinica/Model/Entities/Turno.java
+++ clinica/src/main/java/com/folcademy/clinica/Model/Entities/Turno.java
@@ -2,6 +2,8 @@ package com.folcademy.clinica.Model.Entities;
 
 import lombok.*;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.sql.Time;
@@ -19,7 +21,7 @@ public class Turno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idturno")
-    Integer idturno;
+    Integer idTurno;
 
     @Column(name = "Fecha")
     Date fecha;
@@ -30,13 +32,29 @@ public class Turno {
     @Column(name = "Atendido")
     int atendido;
 
+    @Column(name = "idpaciente")
+    Integer idPaciente;
+
+    @Column(name = "idmedico")
+    Integer idMedico;
+
+    @ManyToOne
+    @NotFound(action = NotFoundAction.IGNORE)
+    @JoinColumn(name = "idpaciente", referencedColumnName = "idpaciente", insertable = false, updatable = false)
+    private Paciente paciente;
+
+    @ManyToOne
+    @NotFound(action = NotFoundAction.IGNORE)
+    @JoinColumn(name = "idmedico", referencedColumnName = "idmedico", insertable = false, updatable = false)
+    private Medico medico;
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         Turno turno = (Turno) o;
-        return idturno != null && Objects.equals(idturno, turno.idturno);
+        return idTurno != null && Objects.equals(idTurno, turno.idTurno);
     }
 
     @Override
