79ca9de9eb63a474839b986a74e24e4a70af3429
Emiliano <emilianocastro97@gmail.com>
2021-12-17 16:34:11 -0300

    🔨Refactorizacion en medico service

--- src/main/java/com/folcademy/clinica/Services/MedicoService.java
+++ src/main/java/com/folcademy/clinica/Services/MedicoService.java
@@ -5,8 +5,10 @@ import com.folcademy.clinica.Exceptions.NotFoundException;
 import com.folcademy.clinica.Model.Dtos.MedicoDto;
 import com.folcademy.clinica.Model.Dtos.MedicoEnteroDto;
 import com.folcademy.clinica.Model.Entities.Medico;
+import com.folcademy.clinica.Model.Entities.Persona;
 import com.folcademy.clinica.Model.Mappers.MedicoMapper;
 import com.folcademy.clinica.Model.Repositories.MedicoRepository;
+import com.folcademy.clinica.Model.Repositories.PersonaRepository;
 import com.folcademy.clinica.Services.Interfaces.IMedicoService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,72 +24,78 @@ import java.util.Objects;
 public class MedicoService implements IMedicoService {
     private final MedicoRepository medicoRepository;
     private final MedicoMapper medicoMapper;
+    private final PersonaRepository personaRepository;
 
-    public MedicoService(MedicoRepository medicoRepository, MedicoMapper medicoMapper, MedicoMapper medicoMapper1) {
+    public MedicoService(MedicoRepository medicoRepository, MedicoMapper medicoMapper, MedicoMapper medicoMapper1, PersonaRepository personaRepository) {
         this.medicoRepository = medicoRepository;
         this.medicoMapper = medicoMapper1;
+        this.personaRepository = personaRepository;
     }
 
 
     @Override
-    public List<MedicoDto> findAllMedicos() {
-        List<MedicoDto> medicos=new ArrayList<>();
+    public List<MedicoEnteroDto> findAllMedicos() {
+        List<MedicoEnteroDto> medicos=new ArrayList<>();
         List<Medico> medicosEntities= (List<Medico>) medicoRepository.findAll();
         if (medicosEntities.isEmpty())
             throw new NotFoundException("No se encontraron medicos");
 
         for (Medico medicoEntity : medicosEntities){
-            medicos.add(medicoMapper.entityToDto(medicoEntity));
+            MedicoEnteroDto dto = medicoMapper.entityToEnteroDto(medicoEntity);
+            dto.setPersona(medicoEntity.getPersona());
+            medicos.add(dto);
         }
 
         return medicos;
-
-
     }
 
     @Override
-    public MedicoDto findMedicoById(Integer id) {
+    public MedicoEnteroDto findMedicoById(Integer id) {
         if (!medicoRepository.existsById(id))
             throw new NotFoundException("No Existe el medico");
-        Medico medicoEntity = medicoRepository.findById(id).orElse(null);
-        MedicoDto medicoDto = medicoMapper.entityToDto(medicoEntity);
-        return medicoDto;
+        Medico medicoEntity = medicoRepository.findById(id).get();
+        MedicoEnteroDto medicoEnteroDto = medicoMapper.entityToEnteroDto(medicoEntity);
+        medicoEnteroDto.setPersona(medicoEntity.getPersona());
+        return medicoEnteroDto;
     }
 
     @Override
-    public MedicoDto save(Medico medico) {
-        medico.setIdmedico(null);
+    public MedicoEnteroDto save(MedicoDto medico) {
+        medico.setId(null);
         if (medico.getConsulta()<0)
             throw new BadRequestException("La consulta no puede ser menor a 0");
-        medicoRepository.save(medico);
-        MedicoDto medicoDto = medicoMapper.entityToDto(medico);
-        return medicoDto;
+        Medico medicoEntity = medicoMapper.dtoToEntity(medico);
+        Persona persona = personaRepository.findById(medico.getId()).get();
+        medicoEntity.setPersona(persona);
+        medicoRepository.save(medicoEntity);
+        MedicoEnteroDto medicoEnteroDto = medicoMapper.entityToEnteroDto(medicoEntity);
+        return medicoEnteroDto;
     }
 
-    public MedicoEnteroDto edit(Integer idMedico, MedicoEnteroDto dto) {
+    public MedicoEnteroDto edit(Integer idMedico, MedicoDto dto) {
         if (!medicoRepository.existsById(idMedico))
             throw new NotFoundException("No existe el medico");
-        dto.setIdmedico(idMedico);
-        return medicoMapper.entityToEnteroDto(
-                    medicoRepository.save(
-                            medicoMapper.enteroDtoToEntity(
-                                    dto
-                            )
-                    )
-        );
+        Medico medicoEntity = medicoRepository.findById(idMedico).get();
+        medicoEntity.setProfesion(dto.getProfesion());
+        medicoEntity.setConsulta(dto.getConsulta());
+        medicoRepository.save(medicoEntity);
+        MedicoEnteroDto medicoEnteroDto = medicoMapper.entityToEnteroDto(medicoEntity);
+        medicoEnteroDto.setPersona(medicoEntity.getPersona());
+        return medicoEnteroDto;
     }
 
-    public MedicoDto delete(Integer id) {
+    public MedicoEnteroDto delete(Integer id) {
         if (!medicoRepository.existsById(id))
             throw new NotFoundException("No existe el medico");
         Medico medicoEntity = medicoRepository.findById(id).get();
-        MedicoDto medicoDto = medicoMapper.entityToDto(medicoEntity);
+        MedicoEnteroDto medicoEnteroDto = medicoMapper.entityToEnteroDto(medicoEntity);
+        medicoEnteroDto.setPersona(medicoEntity.getPersona());
         medicoRepository.deleteById(id);
-        return medicoDto;
+        return medicoEnteroDto;
     }
 
-    public Page<MedicoDto> findAllByPage(Integer pageNumber, Integer pageSize, String orderField) {
-        Pageable pageable = PageRequest.of(pageNumber,pageSize, Sort.by(orderField));
-        return medicoRepository.findAll(pageable).map(medicoMapper::entityToDto);
+    public Page<MedicoEnteroDto> findAllByPage(Integer pageNumber, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber,pageSize);
+        return medicoRepository.findAll(pageable).map(medicoMapper::entityToEnteroDto);
     }
 }
