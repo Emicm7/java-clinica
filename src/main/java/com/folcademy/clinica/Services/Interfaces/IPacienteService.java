a491ec3dc48dd064e2d7ffc2a53067c443b1ead3
Emiliano <emilianocastro97@gmail.com>
2021-12-17 20:09:32 -0300

    🔨Refactorizacion en interfaz Paciente service

--- src/main/java/com/folcademy/clinica/Services/Interfaces/IPacienteService.java
+++ src/main/java/com/folcademy/clinica/Services/Interfaces/IPacienteService.java
@@ -3,12 +3,13 @@ package com.folcademy.clinica.Services.Interfaces;
 import com.folcademy.clinica.Model.Dtos.PacienteDto;
 import com.folcademy.clinica.Model.Dtos.PacienteEnteroDto;
 import com.folcademy.clinica.Model.Entities.Paciente;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface IPacienteService {
 
-    List<PacienteEnteroDto> findAllPacientes();
+    Page<Paciente> findAllByPage(Integer pageNumber, Integer pageSize);
     PacienteEnteroDto findPacienteById(Integer id);
     PacienteEnteroDto save(PacienteDto paciente);
     PacienteEnteroDto edit(Integer idPaciente, PacienteDto dto);
