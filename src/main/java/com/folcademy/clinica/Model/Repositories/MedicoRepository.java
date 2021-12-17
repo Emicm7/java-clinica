d8bf92afeb872e8e199c411c76b063fcf2a1daba
Emiliano <emilianocastro97@gmail.com>
2021-12-17 20:08:36 -0300

    🔥Refactorizacion en Medico Repository

--- src/main/java/com/folcademy/clinica/Model/Repositories/MedicoRepository.java
+++ src/main/java/com/folcademy/clinica/Model/Repositories/MedicoRepository.java
@@ -2,11 +2,11 @@ package com.folcademy.clinica.Model.Repositories;
 
 import com.folcademy.clinica.Model.Entities.Medico;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
-
 @Repository
 public interface MedicoRepository extends PagingAndSortingRepository<Medico, Integer> {
+    Page<Medico> findAll(Pageable pageable);
 }
